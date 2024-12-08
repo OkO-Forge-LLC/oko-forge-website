87d2e39f62425ccdcd7bb8f9052c3faf8c6d952a
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-12-08 15:57:50 -0800

    updates: mvp site and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import {Routes, Route} from "react-router-dom"
-import Header from "./components/Header"
-import Home from "./pages/Home"
-import About from "./pages/About"
-
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-
-
   return (
     <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
-
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
