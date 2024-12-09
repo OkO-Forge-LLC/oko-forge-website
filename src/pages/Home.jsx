0cac4e85f80596a8dd71b2b0f328210562b96838
Zak Ward <zakward85@gmail.com>
2024-12-08 18:05:45 -0600

    just getting started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
-
+import coverImg from "../assets/hammer.png";
+import Header from "../components/Header";
+import "../css/Home.css";
 
 const Home = () => {
-    return (
-        <>
-        Home
-        </>
-    )
-}
+  return (
+    <div
+      className="coverImg-wrapper"
+      style={{ backgroundImage: `url(${coverImg})` }}
+    >
+      <Header />
+      <div className="cover-items">
+        <h1 className="cover-text">OkO-Forge Development & Consulting</h1>
+        <p className="cover-text">Solutions that WORK, Mentorship that INSPIRES</p>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
